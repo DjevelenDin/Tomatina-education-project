884ebcb9b8bffb965e63f3637162145e4c7d796b
DjevelenDin <freelancer5656@gmail.com>
2021-06-10 16:13:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+First education project in the team*
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Tomatina restaurant project!</h1>
+    </div>
+  );
+}
+export default App;
