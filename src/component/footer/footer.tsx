7b5635c865a1570c3887e11812fd8317a1bf2917
NMarta <marta_nagirna11@ukr.net>
2021-07-25 21:01:03 +0300

    footer+icons

--- src/component/footer/footer.tsx
+++ src/component/footer/footer.tsx
@@ -1,51 +1,186 @@
-import React, { Component } from 'react'
-import './footer.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                <div className="app-footer">
-                    <div className="app-footer-block">
-                        <span>Вакансії</span>
-                    </div>
-                    <div className="app-footer-block">
-                        <span>Договір Оферта</span>
-                    </div>
-                    <div className="app-footer-block app-footer-block-map">
-                        <div >
-                            <a className="icons-heart" href="#" ><svg className="bg" width="47" height="45" viewBox="0 0 47 45" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z" stroke="#518B26"></path> </svg>
-                                <i className="fas fa-map-marker-alt"></i></a>
-                        </div>
-                        <div className="app-footer-map">
-                            <p>м.Львів</p>
-                            <p className="app-footer-map-span">ТРЦ Victoria Gardens, Кульпарківська 226 А ТРЦ Spartak, Мазепи 1Б</p>
-                            <p>с. Сокільники</p>
-                            <p className="app-footer-map-span">ТРЦ King Cross, Стрийська 30</p>
-                        </div>
-                    </div>
-                    <div className="app-footer-block app-footer-block-column">
-                        <div className="app-footer-flex">
-                            <a className="icons-heart icons-margin" href="#" ><svg className="bg" width="47" height="45" viewBox="0 0 47 45" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z" stroke="#518B26"></path> </svg>
-                                <i className="fas fa-mobile-alt"></i> </a>
-                            <a href="#">+38 (067) 792 90 90</a>
-                        </div>
-                        <div className="app-footer-flex">
-                            <a className="icons-heart icons-margin" href="#" ><svg className="bg" width="47" height="45" viewBox="0 0 47 45" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z" stroke="#518B26"></path> </svg>
-                                <i className="far fa-clock"></i> </a>
-                            <span>Пн - Нд з 08:30 до 22:00 </span>
-                            <span></span>
-                        </div>
-                    </div>
-                    <div className="app-footer-block app-footer-block-line">
-                        <span>Слідкуй за нами</span>
-                        <a className="icons-heart icons-margin" href="#" ><svg className="bg" width="47" height="45" viewBox="0 0 47 45" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z" stroke="#518B26"></path> </svg>
-                            <i className="fab fa-facebook-f"></i> </a>
-                        <a className="icons-heart icons-margin" href="#" ><svg className="bg" width="47" height="45" viewBox="0 0 47 45" fill="none" xmlns="http://www.w3.org/2000/svg"> <defs> <linearGradient id="myGradient" gradientTransform="rotate(90)"> <stop offset="12.22%" stopColor="#505FD1"></stop> <stop offset="35.67%" stopColor="#CF2D93"></stop> <stop offset="61.16%" stopColor="#F66635"></stop> <stop offset="85.56%" stopColor="#FED16E"></stop> </linearGradient> </defs> <path d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z" stroke=""></path> </svg>
-                            <i className="fab fa-instagram"></i> </a>
-                    </div>
-                </div>
-            </footer>
-        )
-    }
+  render() {
+    return (
+      <footer>
+        <div className="app-footer">
+          <div className="app-footer-block">
+            <span>Вакансії</span>
+          </div>
+          <div className="app-footer-block">
+            <span>Договір Оферта</span>
+          </div>
+          <div className="app-footer-block app-footer-block-map">
+            <div>
+              <Link className="icons-heart" to="#">
+                <svg
+                  className="bg"
+                  width="47"
+                  height="45"
+                  viewBox="0 0 47 45"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  {" "}
+                  <path
+                    d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z"
+                    stroke="#518B26"
+                  ></path>{" "}
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  fill="currentColor"
+                  className="bi bi-geo-alt fas fa-map-marker-alt"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+              </Link>
+            </div>
+            <div className="app-footer-map">
+              <p>м.Львів</p>
+              <p className="app-footer-map-span">
+                ТРЦ Victoria Gardens, Кульпарківська 226 А ТРЦ Spartak, Мазепи
+                1Б
+              </p>
+              <p>с. Сокільники</p>
+              <p className="app-footer-map-span">
+                ТРЦ King Cross, Стрийська 30
+              </p>
+            </div>
+          </div>
+          <div className="app-footer-block app-footer-block-column">
+            <div className="app-footer-flex">
+              <Link className="icons-heart icons-margin" to="#">
+                <svg
+                  className="bg"
+                  width="47"
+                  height="45"
+                  viewBox="0 0 47 45"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  {" "}
+                  <path
+                    d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z"
+                    stroke="#518B26"
+                  ></path>{" "}
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  fill="currentColor"
+                  className="bi bi-phone fas fa-mobile-alt"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg>
+              </Link>
+              <Link to="#">+38 (067) 792 90 90</Link>
+            </div>
+            <div className="app-footer-flex">
+              <Link className="icons-heart icons-margin" to="#">
+                <svg
+                  className="bg"
+                  width="47"
+                  height="45"
+                  viewBox="0 0 47 45"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  {" "}
+                  <path
+                    d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z"
+                    stroke="#518B26"
+                  ></path>{" "}
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  fill="currentColor"
+                  className=" far fa-clock bi bi-clock"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                </svg>
+              </Link>
+              <span>Пн - Нд з 08:30 до 22:00 </span>
+              <span></span>
+            </div>
+          </div>
+          <div className="app-footer-block app-footer-block-line">
+            <span>Слідкуй за нами</span>
+            <Link className="icons-heart icons-margin" to="#">
+              <svg
+                className="bg bg-fb"
+                width="47"
+                height="45"
+                viewBox="0 0 47 45"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {" "}
+                <path
+                  className="bg-fb"
+                  d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z"
+                  stroke="#518B26"
+                ></path>{" "}
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="currentColor"
+                className="fab fa-facebook-f bi bi-facebook"
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+              </svg>
+            </Link>
+            <Link className="icons-heart icons-margin" to="#">
+              <svg
+                className="bg bg-insta"
+                width="47"
+                height="45"
+                viewBox="0 0 47 45"
+                fill="url(#MyGradient)"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M45.2083 18.1669L45.2084 18.167C46.7906 22.9887 47.0613 28.4122 45.2187 33.0569C42.3923 40.1815 34.8975 44.3582 27.6059 44.4963C21.8566 44.6044 14.3579 42.3387 8.69888 38.1334C3.05046 33.9361 -0.68529 27.8778 0.845276 20.378C2.16342 13.9239 7.85576 7.82595 13.0413 4.37013C19.7859 -0.122065 26.5525 -0.480618 32.2513 2.14746C37.9685 4.78402 42.6817 10.4605 45.2083 18.1669Z"
+                  stroke=""
+                ></path>{" "}
+                <rect
+                  width="47"
+                  height="45"
+                  fill="url(#myGradient)"
+                  viewBox="0 0 47 45"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="currentColor"
+                className="fab fa-instagram bi bi-instagram"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
